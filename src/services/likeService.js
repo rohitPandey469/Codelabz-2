44ed1b534e9b8d98f9dc32a17e53da775b17d8c9
rohitPandey469 <rd8614196@gmail.com>
2024-03-21 19:03:59 +0530

    Changes made

--- src/services/likeService.js
+++ src/services/likeService.js
@@ -1,4 +1,8 @@
-import {checkExistingFeedback,undoVote,updateUpvote} from "../store/actions";
+import {
+  checkExistingFeedback,
+  undoVote,
+  updateUpvote
+} from "../store/actions";
 
 const handleLike = async (firebase, firestore, dispatch, itemId, itemType) => {
   try {
@@ -7,20 +11,42 @@ const handleLike = async (firebase, firestore, dispatch, itemId, itemType) => {
     if (!currentUser) {
       return;
     }
-    const existingFeedback = await checkExistingFeedback(userId,itemId,`${itemType}_likes`)(firebase, firestore);
+    const existingFeedback = await checkExistingFeedback(
+      userId,
+      itemId,
+      `${itemType}_likes`
+    )(firebase, firestore);
     if (existingFeedback === 1) {
       // User already liked - undo like
       console.log("Undo upVote");
-      await undoVote(userId,itemId,itemType,existingFeedback)(firebase, firestore, dispatch); // removed the like
+      await undoVote(
+        userId,
+        itemId,
+        itemType,
+        existingFeedback
+      )(firebase, firestore, dispatch); // removed the like
     } else if (existingFeedback === -1) {
       // User disliked - change dislike to like
       console.log("Change  Dislike To Like");
-      await undoVote(userId,itemId,itemType,existingFeedback)(firebase, firestore, dispatch); // removed the dislike
-      await updateUpvote(userId, itemId, itemType)(firebase,firestore,dispatch); // liked
+      await undoVote(
+        userId,
+        itemId,
+        itemType,
+        existingFeedback
+      )(firebase, firestore, dispatch); // removed the dislike
+      await updateUpvote(userId, itemId, itemType)(
+        firebase,
+        firestore,
+        dispatch
+      ); // liked
     } else {
       // No previous record - add a new like
       console.log("Just add a normal like");
-      await updateUpvote(userId, itemId, itemType)(firebase,firestore,dispatch);
+      await updateUpvote(userId, itemId, itemType)(
+        firebase,
+        firestore,
+        dispatch
+      );
     }
   } catch (error) {
     console.log("Error handling Like:", error);
